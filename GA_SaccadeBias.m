92c6b58795dab48d8775d9280955bd8c4aeb839d
Anna <anna@vanharmelen.me>
2025-03-04 14:49:08 +0100

    init saccade analysis - initial look at data, all comps

---
+++ GA_SaccadeBias.m
@@ -0,0 +1,246 @@
+
+%% Step3b--grand average plots of gaze-shift (saccade) results
+
+%% start clean
+clear; clc; close all;
+    
+%% parameters
+oneOrTwoD       = 1;
+oneOrTwoD_options = {'_1D','_2D'};
+
+pp2do           = [2:9];
+
+nsmooth         = 500;
+plotSinglePps   = 0;
+plotGAs         = 1;
+xlimtoplot      = [-750 1500];
+
+%% visual parameters
+[bar_size, bright_colours, colours, light_colours, SOA_colours, dark_colours, subplot_size] = setBehaviourParam(pp2do);
+
+%% load and aggregate the data from all pp
+s = 0;
+for pp = pp2do
+    s = s+1;
+
+    % get participant data
+    param = getSubjParam(pp);
+
+    % load
+    disp(['getting data from participant ', param.subjName]);
+    load([param.path, '\saved_data\saccadeEffects', oneOrTwoD_options{oneOrTwoD} '__', param.subjName], 'saccade','saccadesize');
+    
+    % smooth?
+    if nsmooth > 0
+        for i = 1:size(saccade.toward,1)
+            saccade.toward(i,:)  = smoothdata(squeeze(saccade.toward(i,:)), 'gaussian', nsmooth);
+            saccade.away(i,:)    = smoothdata(squeeze(saccade.away(i,:)), 'gaussian', nsmooth);
+            saccade.effect(i,:)  = smoothdata(squeeze(saccade.effect(i,:)), 'gaussian', nsmooth);
+        end
+
+        %also smooth saccadesize data over time.
+        for i = 1:size(saccadesize.toward,1)
+            for j = 1:size(saccadesize.toward,2)
+                saccadesize.toward(i,j,:) = smoothdata(squeeze(saccadesize.toward(i,j,:)), 'gaussian', nsmooth);
+                saccadesize.away(i,j,:)   = smoothdata(squeeze(saccadesize.away(i,j,:)), 'gaussian', nsmooth);
+                saccadesize.effect(i,j,:) = smoothdata(squeeze(saccadesize.effect(i,j,:)), 'gaussian', nsmooth);
+            end
+        end
+    end
+
+    % put into matrix, with pp as first dimension
+    d1(s,:,:) = saccade.toward;
+    d2(s,:,:) = saccade.away;
+    d3(s,:,:) = saccade.effect;
+
+    d4(s,:,:,:) = saccadesize.toward;
+    d5(s,:,:,:) = saccadesize.away;
+    d6(s,:,:,:) = saccadesize.effect;
+end
+
+%% make GA for the saccadesize fieldtrip structure data, to later plot as "time-frequency map" with fieldtrip. For timecourse data, we directly plot from d structures above. 
+saccadesize.toward = squeeze(mean(d4));
+saccadesize.away   = squeeze(mean(d5));
+saccadesize.effect = squeeze(mean(d6));
+
+%% all subs
+if plotSinglePps
+    % toward & away - some cue
+    figure;
+    for sp = 1:s
+        subplot(subplot_size,subplot_size,sp); hold on;
+        plot(saccade.time, squeeze(d1(sp,7,:)));
+        plot(saccade.time, squeeze(d2(sp,7,:)));
+        plot(xlim, [0,0], '--k');
+        xlim(xlimtoplot);
+        % ylim([-0.5 0.5]);
+        title(pp2do(sp));
+    end
+    legend({'toward', 'away'});
+
+    % toward vs away - all
+    figure;
+    for sp = 1:s
+        subplot(subplot_size,subplot_size,sp); hold on;
+        plot(saccade.time, squeeze(d3(sp,7,:)));
+        plot(xlim, [0,0], '--k');
+        xlim(xlimtoplot);
+        % ylim([-0.5 0.5]);
+        title(pp2do(sp));
+    end
+    legend({'all'});
+
+    % toward vs. away - cue0 vs. cueany
+    figure;
+    for sp = 1:s
+        subplot(subplot_size,subplot_size,sp); hold on;
+        plot(saccade.time, squeeze(d3(sp,4,:)));
+        plot(saccade.time, squeeze(d3(sp,7,:)));
+        plot(xlim, [0,0], '--k');
+        xlim(xlimtoplot);
+        % ylim([-0.5 0.5]);
+        title(pp2do(sp));
+    end
+    legend({'cue0', 'cue 1 or 2'});
+
+    % towardness for all conditions condition - gaze shift effect X saccade size
+    figure;
+    for sp = 1:s
+        subplot(subplot_size,subplot_size,sp);
+        cfg = [];
+        cfg.parameter = 'effect';
+        cfg.figure = 'gcf';
+        cfg.zlim = [-.1 .1];
+        cfg.xlim = xlimtoplot;
+        for sp = 1:s
+            subplot(subplot_size,subplot_size,sp); hold on;
+            saccadesize.effect = squeeze(d6(sp,:,:,:)); % put in data from this pp
+            cfg.channel = 7; % all conditions combined.
+            ft_singleplotTFR(cfg, saccadesize);
+            title(pp2do(sp));
+        end
+        colormap('jet');
+    end
+end
+
+%% Plot grand average data patterns of interest, with error bars
+if plotGAs
+    % plot toward, away and effect - all
+    figure; 
+    subplot(2,1,1)
+    hold on
+    p1 = frevede_errorbarplot(saccade.time, squeeze(d1(:,7,:)), 'b', 'se');
+    p2 = frevede_errorbarplot(saccade.time, squeeze(d2(:,7,:)), 'r', 'se');
+    legend([p1, p2], {'toward', 'away'});
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    hold off
+    subplot(2,1,2)
+    p3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,7,:)), 'k', 'se');
+    plot(xlim,[0 0]);
+    
+    % plot main combination
+    figure; 
+    subplot(3,2,1)
+    hold on
+    p5 = frevede_errorbarplot(saccade.time, squeeze(d1(:,10,:)), 'b', 'se');
+    p6 = frevede_errorbarplot(saccade.time, squeeze(d2(:,10,:)), 'r', 'se');
+    legend([p5, p6], {'toward', 'away'});
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    title('Cue not zero - same side')
+    hold off
+
+    subplot(3,2,3)
+    hold on
+    p5 = frevede_errorbarplot(saccade.time, squeeze(d1(:,8,:)), 'b', 'se');
+    p6 = frevede_errorbarplot(saccade.time, squeeze(d2(:,8,:)), 'r', 'se');
+    legend([p5, p6], {'toward', 'away'});
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    title('Cue zero - same side')
+    hold off
+
+    subplot(3,2,5)
+    hold on
+    p7 = frevede_errorbarplot(saccade.time, squeeze(d3(:,8,:)), 'c', 'se');
+    p8 = frevede_errorbarplot(saccade.time, squeeze(d3(:,10,:)), 'm', 'se');
+    legend([p7, p8], {'cue zero', 'cue not zero'});
+    plot(xlim, [0 0],'k--')
+    title('effect - same side');
+    hold off
+    
+    subplot(3,2,2)
+    hold on
+    p5 = frevede_errorbarplot(saccade.time, squeeze(d1(:,11,:)), 'b', 'se');
+    p6 = frevede_errorbarplot(saccade.time, squeeze(d2(:,11,:)), 'r', 'se');
+    legend([p5, p6], {'toward', 'away'});
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    title('Cue not zero - other side')
+    hold off
+
+    subplot(3,2,4)
+    hold on
+    p5 = frevede_errorbarplot(saccade.time, squeeze(d1(:,9,:)), 'b', 'se');
+    p6 = frevede_errorbarplot(saccade.time, squeeze(d2(:,9,:)), 'r', 'se');
+    legend([p5, p6], {'toward', 'away'});
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    title('Cue zero - other side')
+    hold off
+
+    subplot(3,2,6)
+    hold on
+    p7 = frevede_errorbarplot(saccade.time, squeeze(d3(:,9,:)), 'c', 'se');
+    p8 = frevede_errorbarplot(saccade.time, squeeze(d3(:,11,:)), 'm', 'se');
+    plot(xlim, [0 0],'k--')
+    legend([p7, p8], {'cue zero', 'cue not zero'});
+    title('effect - other side');
+    hold off
+
+    % plot the effect - cueany same vs. other side
+    figure;
+    hold on
+    p9 = frevede_errorbarplot(saccade.time, squeeze(d3(:,10,:)), 'r', 'se');
+    p10 = frevede_errorbarplot(saccade.time, squeeze(d3(:,11,:)), 'm', 'se');
+    legend([p9, p10], {'cue any - same side', 'cue any - other side'});
+    plot(xlim, [0 0]);
+    ylabel('Hz')
+    title('effect (toward vs. away) - same vs. other side (only cued)');
+    hold off
+
+    % plot the effect - cueL vs. cueR
+    figure;
+    hold on
+    p7 = frevede_errorbarplot(saccade.time, squeeze(d3(:,2,:)), 'c', 'se');
+    p8 = frevede_errorbarplot(saccade.time, squeeze(d3(:,3,:)), 'm', 'se');
+    xlim(xlimtoplot);
+    plot(xlim, [0,0], '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+    plot([0,0], ylim, '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+    hold off
+    
+    %% just effect as function of saccade size
+    cfg = [];
+    cfg.parameter = 'effect';
+    cfg.figure = 'gcf';
+    cfg.zlim = [-0.3, 0.3];
+    cfg.xlim = xlimtoplot;  
+    cfg.colormap = 'jet';
+    
+    % per condition
+    figure;
+    for chan = [1:9]
+        hold on
+        cfg.channel = chan;
+        subplot(3,3,chan);
+        saccadesize.effect = squeeze(d6(sp,:,:,:)); % put in data from all pp
+        ft_singleplotTFR(cfg, saccadesize);
+        ylabel('Saccade size (dva)');
+        xlabel('Time (ms)');
+        xlim(xlimtoplot);
+    end
+  
+end
---
+++ get_SaccadeBias.m
@@ -0,0 +1,231 @@
+%% Step3-- gaze-shift calculation
+
+%% start clea
+clear; clc; close all;
+
+%% parameter
+oneOrTwoD  = 1;
+oneOrTwoD_options = {'_1D','_2D'};
+
+plotResults = 0;
+
+%% loop over participants
+for pp = [1:9];
+
+    %% load epoched data of this participant data
+    param = getSubjParam(pp);
+    load([param.path, '\epoched_data\eyedata_m3', '__', param.subjName], 'eyedata');
+
+    %% only keep channels of interest
+    cfg = [];
+    cfg.channel = {'eyeX','eyeY'}; % only keep x & y axis
+    eyedata = ft_selectdata(cfg, eyedata); % select x & y channels
+
+    %% reformat all data to a single matrix of trial x channel x time
+    cfg = [];
+    cfg.keeptrials = 'yes';
+    tl = ft_timelockanalysis(cfg, eyedata); % realign the data: from trial*time cells into trial*channel*time?
+    tl.time = tl.time * 1000;
+
+    %% pixel to degree
+    [dva_x, dva_y] = frevede_pixel2dva(squeeze(tl.trial(:,1,:)), squeeze(tl.trial(:,2,:)));
+    tl.trial(:,1,:) = dva_x;
+    tl.trial(:,2,:) = dva_y;
+
+    %% selection vectors for conditions -- this is where it starts to become interesting!
+    % select possible target locations
+    targL = ismember(tl.trialinfo(:,1), [31,32,33,36,311,312,313,316]);
+    targR = ismember(tl.trialinfo(:,1), [34,35,37,38,314,315,317,318]);
+    
+    % cue can be many different things
+    cueL = ismember(tl.trialinfo(:,1), [31,32,33,36]);
+    cueR = ismember(tl.trialinfo(:,1), [34,35,37,38]);
+    cue0 = ismember(tl.trialinfo(:,1), [311:318]);
+    cue1 = ismember(tl.trialinfo(:,1), [31,33,35,37]);
+    cue2 = ismember(tl.trialinfo(:,1), [32,34,36,38]);
+      
+    % when was target presented
+    targ1 = ismember(tl.trialinfo(:,1), [31,33,35,37,311,313,315,317]);
+    targ2 = ismember(tl.trialinfo(:,1), [32,34,36,38,312,314,316,318]);
+    
+    % same side or not
+    same_side = ismember(tl.trialinfo(:,1), [31,32,37,38,311,312,317,318]);
+    other_side = ismember(tl.trialinfo(:,1), [33,34,35,36,313,314,315,316]);
+
+    % channels
+    chX = ismember(tl.label, 'eyeX');
+    chY = ismember(tl.label, 'eyeY');
+
+    %% get gaze shifts using our custom function
+    cfg = [];
+    data_input = squeeze(tl.trial);
+    time_input = tl.time;
+
+    [shiftsX,shiftsY, peakvelocity, times] = PBlab_gazepos2shift_2D(cfg, data_input(:,chX,:), data_input(:,chY,:), time_input);
+
+    %% select usable gaze shifts
+    minDisplacement = 0;
+    maxDisplacement = 1000;
+
+    if oneOrTwoD == 1
+        saccadesize = abs(shiftsX);
+    elseif oneOrTwoD == 2
+        saccadesize = abs(shiftsX+shiftsY*1i);
+    end
+
+    shiftsL = shiftsX<0 & (saccadesize>minDisplacement & saccadesize<maxDisplacement);
+    shiftsR = shiftsX>0 & (saccadesize>minDisplacement & saccadesize<maxDisplacement);
+
+    %% get relevant contrasts out
+    saccade = [];
+    saccade.time = times;
+    sel = ones(size(cueL)); %NB: selection of oktrials has happened at the start when remove_unfixated is "on".
+    saccade.label = {'all','cueL','cueR','cue0','cue1','cue2','cueany','same_side_0','other_side_0','same_side_any','other_side_any'};
+
+    for selection = [1:11] % conditions.
+        if     selection == 1  sel = ones(size(targL));
+        elseif selection == 2  sel = cueL;
+        elseif selection == 3  sel = cueR;
+        elseif selection == 4  sel = cue0;
+        elseif selection == 5  sel = cue1;
+        elseif selection == 6  sel = cue2;
+        elseif selection == 7  sel = logical(cue1+cue2);
+        elseif selection == 8  sel = same_side&cue0;
+        elseif selection == 9  sel = other_side&cue0;
+        elseif selection == 10  sel = same_side&logical(cue1+cue2);
+        elseif selection == 11  sel = other_side&logical(cue1+cue2);
+        end
+        
+        if selection == 2
+            saccade.toward(selection,:) =  mean(shiftsL(sel,:));
+            saccade.away(selection,:)  =   mean(shiftsR(sel,:));
+        elseif selection == 3
+            saccade.toward(selection,:) =  mean(shiftsR(sel,:));
+            saccade.away(selection,:)  =   mean(shiftsL(sel,:));
+        else
+            saccade.toward(selection,:) =  (mean(shiftsL(targL&sel,:)) + mean(shiftsR(targR&sel,:))) ./ 2;
+            saccade.away(selection,:)  =   (mean(shiftsL(targR&sel,:)) + mean(shiftsR(targL&sel,:))) ./ 2;
+        end
+    end
+
+    % add towardness field
+    saccade.effect = (saccade.toward - saccade.away);
+    
+    %% smooth and turn to Hz
+    integrationwindow = 100; % window over which to integrate saccade counts
+    
+    saccade.toward = smoothdata(saccade.toward,2,'movmean',integrationwindow)*1000; % *1000 to get to Hz, given 1000 samples per second.
+    saccade.away   = smoothdata(saccade.away,2,  'movmean',integrationwindow)*1000;
+    saccade.effect = smoothdata(saccade.effect,2,'movmean',integrationwindow)*1000;
+       
+    %% plot
+    if plotResults
+        figure; 
+        hold on
+        plot(saccade.time, saccade.toward(1,:,:), 'b');
+        plot(saccade.time, saccade.away(1,:,:), 'r');
+        plot(saccade.time, saccade.effect(1,:,:), 'k');
+        title('Main effect');
+        hold off
+
+        figure; 
+        hold on
+        plot(saccade.time, saccade.toward(4,:,:), 'b');
+        plot(saccade.time, saccade.away(4,:,:), 'r');
+        plot(saccade.time, saccade.effect(4,:,:), 'k');
+        title('Effect of null cue');
+        hold off
+        
+        figure; 
+        hold on
+        plot(saccade.time, saccade.toward(7,:,:), 'b');
+        plot(saccade.time, saccade.away(7,:,:), 'r');
+        plot(saccade.time, saccade.effect(7,:,:), 'k');
+        title('Effect of any cue');
+        hold off
+
+        figure;
+        hold on
+        plot(saccade.time, saccade.effect(5,:,:), 'c');
+        plot(saccade.time, saccade.effect(6,:,:), 'm');
+        title('First vs. second item - effect');
+        legend(saccade.label(5:6));
+        hold off
+    end
+
+    %% also get as function of saccade size - identical as above, except with extra loop over saccade size.
+    binsize = 0.5;
+    halfbin = binsize/2;
+
+    saccadesize = [];
+    saccadesize.dimord = 'chan_freq_time';
+    saccadesize.freq = halfbin:0.1:7-halfbin; % shift sizes, as if "frequency axis" for time-frequency plot
+    saccadesize.time = times;
+    saccadesize.label = saccade.label;
+
+    c = 0;
+    for sz = saccadesize.freq;
+        c = c+1;
+        
+        shiftsL = [];
+        shiftsR = [];
+        shiftsL = shiftsX<-sz+halfbin & shiftsX > -sz-halfbin; % left shifts within this range
+        shiftsR = shiftsX>sz-halfbin  & shiftsX < sz+halfbin; % right shifts within this range
+
+        for selection = [1:11] % conditions.
+            if     selection == 1  sel = ones(size(targL));
+            elseif selection == 2  sel = cueL;
+            elseif selection == 3  sel = cueR;
+            elseif selection == 4  sel = cue0;
+            elseif selection == 5  sel = cue1;
+            elseif selection == 6  sel = cue2;
+            elseif selection == 7  sel = logical(cue1+cue2);
+            elseif selection == 8  sel = same_side&cue0;
+            elseif selection == 9  sel = other_side&cue0;
+            elseif selection == 10  sel = same_side&logical(cue1+cue2);
+            elseif selection == 11  sel = other_side&logical(cue1+cue2);
+            end
+            
+            if selection == 2
+                saccadesize.toward(selection,c,:) =  mean(shiftsL(sel,:));
+                saccadesize.away(selection,c,:)  =   mean(shiftsR(sel,:));
+            elseif selection == 3
+                saccadesize.toward(selection,c,:) =  mean(shiftsR(sel,:));
+                saccadesize.away(selection,c,:)  =   mean(shiftsL(sel,:));
+            else
+                saccadesize.toward(selection,c,:) = (mean(shiftsL(targL&sel,:)) + mean(shiftsR(targR&sel,:))) ./ 2;
+                saccadesize.away(selection,c,:) =   (mean(shiftsL(targR&sel,:)) + mean(shiftsR(targL&sel,:))) ./ 2;
+            end
+        end
+
+    end
+   
+    % add towardness field
+    saccadesize.effect = (saccadesize.toward - saccadesize.away);
+
+    %% smooth and turn to Hz
+    integrationwindow = 100; % window over which to integrate saccade counts
+    saccadesize.toward = smoothdata(saccadesize.toward,3,'movmean',integrationwindow)*1000; % *1000 to get to Hz, given 1000 samples per second.
+    saccadesize.away   = smoothdata(saccadesize.away,3,  'movmean',integrationwindow)*1000;
+    saccadesize.effect = smoothdata(saccadesize.effect,3,'movmean',integrationwindow)*1000;
+    
+    %% plot saccadesize effects
+    if plotResults
+        cfg = [];
+        cfg.parameter = 'effect';
+        cfg.figure = 'gcf';
+        cfg.zlim = [-0.5, 0.5];
+        figure;
+        for chan = 1:7
+            cfg.channel = chan;
+            subplot(3,3,chan);
+            ft_singleplotTFR(cfg, saccadesize);
+        end
+        colormap('jet');
+        drawnow;
+    end
+
+    %% save
+    save([param.path, '\saved_data\saccadeEffects', oneOrTwoD_options{oneOrTwoD} '__', param.subjName], 'saccade','saccadesize');
+    %% close loops
+end % end pp loop
